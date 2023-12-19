9faa0dabe6e688842baa97c986bee740528250d3
CryptoKeyPlayer <62218123+SCIFER99@users.noreply.github.com>
2023-12-19 14:46:52 -0500

    Files Uploaded!!!

---
+++ mario_level_1.py
@@ -0,0 +1,12 @@
+# This is an attempt to recreate the first level of
+# Super Mario Bros for the NES.
+
+import sys
+import pygame as pg
+from data.main import main
+import cProfile
+
+if __name__ == '__main__':
+    main()
+    pg.quit()
+    sys.exit()
